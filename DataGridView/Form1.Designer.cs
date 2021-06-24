b3fe87c3af7f5472f1f6ae187e92815fa3f2a214
Jazhiel Arturo Cossío López <jazhiel12a@gmail.com>
2021-06-24 00:23:40 -0500

    Initial commit

---
+++ DataGridView/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DG1 = new System.Windows.Forms.DataGridView();
+            this.NAME1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LASTNAME1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.U1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.U2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.U3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AVERAGE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DG1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NAME";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(356, 79);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 36);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(357, 154);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(253, 36);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 151);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 34);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "LASTNAME";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 514);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 49);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DG1
+            // 
+            this.DG1.AllowUserToAddRows = false;
+            this.DG1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DG1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DG1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DG1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NAME1,
+            this.LASTNAME1,
+            this.U1,
+            this.U2,
+            this.U3,
+            this.AVERAGE});
+            this.DG1.Enabled = false;
+            this.DG1.Location = new System.Drawing.Point(71, 288);
+            this.DG1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DG1.Name = "DG1";
+            this.DG1.ReadOnly = true;
+            this.DG1.Size = new System.Drawing.Size(856, 181);
+            this.DG1.TabIndex = 5;
+            this.DG1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // NAME1
+            // 
+            this.NAME1.HeaderText = "NAME1";
+            this.NAME1.Name = "NAME1";
+            this.NAME1.ReadOnly = true;
+            // 
+            // LASTNAME1
+            // 
+            this.LASTNAME1.HeaderText = "LASTNAME1";
+            this.LASTNAME1.Name = "LASTNAME1";
+            this.LASTNAME1.ReadOnly = true;
+            // 
+            // U1
+            // 
+            this.U1.HeaderText = "U1";
+            this.U1.Name = "U1";
+            this.U1.ReadOnly = true;
+            // 
+            // U2
+            // 
+            this.U2.HeaderText = "U2";
+            this.U2.Name = "U2";
+            this.U2.ReadOnly = true;
+            // 
+            // U3
+            // 
+            this.U3.HeaderText = "U3";
+            this.U3.Name = "U3";
+            this.U3.ReadOnly = true;
+            // 
+            // AVERAGE
+            // 
+            this.AVERAGE.HeaderText = "AVERAGE";
+            this.AVERAGE.Name = "AVERAGE";
+            this.AVERAGE.ReadOnly = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(852, 54);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(72, 36);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(748, 127);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 34);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "U2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(852, 124);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(73, 36);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(748, 52);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 34);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "U1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(747, 196);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 34);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "U3";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(851, 193);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(73, 36);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox5_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(935, 330);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(261, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "EL PROMEDIO DEL GRUPO ES:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(949, 384);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 23);
+            this.label7.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1503, 587);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.DG1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DG1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView DG1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NAME1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LASTNAME1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn U1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn U2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn U3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AVERAGE;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ DataGridView/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace DataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ave, ave1;
+            string txt1,txt2,txt3,txt4,txt5,txt6,ERROR="";
+            int cal1, cal2, cal3;
+
+            txt1 = textBox1.Text;
+            txt2 = textBox2.Text;
+            txt3 =textBox3.Text;
+            txt4= textBox4.Text;
+            txt5= textBox5.Text;
+            textBox1.BackColor = Color.White;
+            textBox2.BackColor = Color.White;
+            textBox3.BackColor = Color.White;
+            textBox4.BackColor = Color.White;
+            textBox5.BackColor = Color.White;
+
+
+
+
+            if (string.IsNullOrWhiteSpace(txt1))
+            {
+                ERROR += " NO PUEDES DEJAR VACIO EL CAMPO NAME";
+                textBox1.BackColor = Color.DarkRed;
+
+            }
+
+
+
+            if (string.IsNullOrWhiteSpace(txt2))
+            {
+                ERROR += " NO PUEDES DEJAR VACIO EL CAMPO LAST NAME";
+                textBox2.BackColor = Color.DarkRed;
+
+            }
+            if (string.IsNullOrWhiteSpace(txt3))
+            {
+                ERROR += " NO PUEDES DEJAR VACIO EL CAMPO U1";
+                textBox3.BackColor = Color.DarkRed;
+
+            }
+            if (string.IsNullOrWhiteSpace(txt4))
+            {
+                ERROR += " NO PUEDES DEJAR VACIO EL CAMPO U2";
+                textBox4.BackColor = Color.DarkRed;
+
+            }
+            if (string.IsNullOrWhiteSpace(txt5))
+            {
+                ERROR += " NO PUEDES DEJAR VACIO EL CAMPO U3";
+                textBox5.BackColor = Color.DarkRed;
+
+            }
+
+            if (ERROR != "")
+            {
+                MessageBox.Show(ERROR, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+
+           if(Convert.ToInt32(txt3)>100 || (Convert.ToInt32(txt3))<0){
+
+
+               ERROR += " SOLO PUEDES INTRODUCIR VALORES ENTRE 0 Y 100";
+               textBox3.BackColor = Color.DarkRed;
+           }
+           if (Convert.ToInt32(txt4) > 100 || (Convert.ToInt32(txt4)) < 0)
+           {
+
+
+               ERROR += " SOLO PUEDES INTRODUCIR VALORES ENTRE 0 Y 100";
+               textBox4.BackColor = Color.DarkRed;
+           }
+
+           if (Convert.ToInt32(txt5) > 100 || (Convert.ToInt32(txt5)) < 0)
+           {
+
+
+               ERROR += " SOLO PUEDES INTRODUCIR VALORES ENTRE 0 Y 100";
+               textBox5.BackColor = Color.DarkRed;
+           }
+
+           if (ERROR != "")
+           {
+               MessageBox.Show(ERROR, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+               return;
+           }
+
+
+
+
+            ave = (Convert.ToDouble(txt3) + Convert.ToDouble(txt4) + Convert.ToDouble(txt5)) / 3;
+            txt6 = ave.ToString();
+            //AGREGAR RENGLONES AL DATAGRIDVIEW
+            DG1.Rows.Add(txt1, txt2,txt3,txt4,txt5,txt6);
+
+
+            int totalu = 0;
+            double prom = 0;
+           double prom2 = 0;
+            foreach (DataGridViewRow row1 in DG1.Rows)
+            {
+                totalu++;
+      
+                    cal1 = Convert.ToInt32(DG1[2, row1.Index].Value);
+                    cal2 = Convert.ToInt32(DG1[3, row1.Index].Value);
+                    cal3 = Convert.ToInt32(DG1[4, row1.Index].Value);
+                   ave1 = Convert.ToDouble(DG1[5, row1.Index].Value);
+                   prom2 = ave1;
+                    if (cal1 < 80)
+                    {
+                        DG1[2, row1.Index].Style.BackColor = Color.Red;
+                    }
+
+                    if (cal2 < 80)
+                    {
+                        DG1[3, row1.Index].Style.BackColor = Color.Red;
+                    }
+                    if (cal3 < 80)
+                    {
+                        DG1[4, row1.Index].Style.BackColor = Color.Red;
+                    }
+                    if (ave1 < 80)
+                    {
+                        DG1[5, row1.Index].Style.BackColor = Color.Red;
+                    }
+
+
+
+                    prom += prom2 / totalu;
+            }
+      
+            label7.Text = Convert.ToString(prom);
+            }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = (char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Space);
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = (char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Space);
+        }
+
+        private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = (char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Space);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Space || e.KeyChar == (char)Keys.Back);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Space || e.KeyChar == (char)Keys.Back);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
